4d86733a49924187a03ca2df5c5d730a2bb1cdf0
Young1721 <gksdud1721@gmail.com>
2024-03-14 11:53:01 +0900

    Update 자연수 뒤집어 배열로 만들기.java

--- programmers/자연수 뒤집어 배열로 만들기.java
+++ programmers/자연수 뒤집어 배열로 만들기.java
@@ -5,6 +5,15 @@ public class Solution {
         char[] charArr = s.toCharArray();
         int [] answer = new int[s.length()];
         int end = 0;
+        //StringBuffer / reverse() 메소드 사용하면 문자열 뒤집기 가능
+        //String str = "ABCDE";         
+        // 문자열 reverse        
+        //StringBuffer sb = new StringBuffer(str);        
+        //String reverse = sb.reverse().toString();         
+        // 결과 출력        
+        //System.out.println(sb); // "EDCBA"        
+        //System.out.println(reverse); // "EDCBA"
+
         for (int i = charArr.length; i > 0; i--){
             answer[end] = Character.getNumericValue(charArr[i-1]);
             end++;
